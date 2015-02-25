1c68001b5a6ba7433bbe49f9884572268b9f9212
sadhav <sadhav@nisum.com>
2015-02-25 19:09:10 +0530

    Second Commmit

--- SpringMVC/src/main/java/org/nisum/constants/EmpRestURIConstants.java
+++ SpringMVC/src/main/java/org/nisum/constants/EmpRestURIConstants.java
@@ -7,4 +7,9 @@ public class EmpRestURIConstants {
     public static final String GET_ALL_EMP = "/rest/emps";
     public static final String CREATE_EMP = "/rest/emp/create";
     public static final String DELETE_EMP = "/rest/emp/delete/{name}";
+    
+    
+    
+    
+    
 }
